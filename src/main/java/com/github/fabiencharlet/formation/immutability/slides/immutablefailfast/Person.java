fa53e8e5dc37e357d598c6915da10d2ad58f0f1b
Fabien <fabien.charlet@gmail.com>
2020-03-24 16:26:30 +0100

    Add fail fast

---
+++ src/main/java/com/github/fabiencharlet/formation/immutability/slides/immutablefailfast/Person.java
@@ -0,0 +1,58 @@
+package com.github.fabiencharlet.formation.immutability.slides.immutablefailfast;
+
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.google.common.base.Strings;
+
+public final class Person {
+
+	public static Person withOneMail(String name, String mail) {
+
+		return new Person(name, Arrays.asList(mail));
+	}
+
+	private final String name;
+	private final List<String> mails;
+
+	private Person(String name, List<String> mails) {
+
+		if (Strings.isNullOrEmpty(name)) {
+			throw new InvalidParameterException("Name is empty");
+		}
+
+		if (mails == null || mails.isEmpty()) {
+			throw new InvalidParameterException("No mails supplied");
+		}
+
+		this.name = name;
+		this.mails = Collections.unmodifiableList(mails);
+	}
+	public Person addMail(String mail) {
+
+		if (Strings.isNullOrEmpty(mail)) {
+			throw new InvalidParameterException("Mail is empty");
+		}
+
+		final List<String> newMails = new ArrayList<>(mails);
+		newMails.add(mail);
+		return new Person(name, newMails);
+	}
+	public String getName() {
+		return name;
+	}
+	public List<String> getMails() {
+		return mails;
+	}
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+
+		final Person fabien = Person
+				.withOneMail("Fabien", "fabien@example.org")
+				.addMail("fabien@example.com");
+	}
+}
